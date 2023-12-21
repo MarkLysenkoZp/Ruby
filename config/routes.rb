9b1e4b622013914992ef0b79c77b5044d8fb24c0
MarkLysenkoZp <marcuslysenko061@gmail.com>
2023-12-21 14:22:14 +0200

    final part

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get  'static_pages/contact'
+  get 'static_pages/home',    to: 'static_pages#home',    as: 'home'
+  get 'static_pages/help',    to: 'static_pages#help',    as: 'help'
+  get 'static_pages/about',   to: 'static_pages#about',   as: 'about'
+  get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
+  get 'signup', to: 'users#new', as: 'signup'
 
 
   resources :microposts
