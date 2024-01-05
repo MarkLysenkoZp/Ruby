d70bfc7abcfbe7ff69568bb15e29c9a74e4d6c28
MarkLysenkoZp <marcuslysenko061@gmail.com>
2024-01-05 18:11:39 +0200

    Exercise 2 update

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,24 +2,23 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "valid signup information" do
+  test "invalid signup information" do
     get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger', text: 'The form contains 4 errors.'
     assert_select 'ul' do
-      assert_select 'li', count: 5
+      assert_select 'li', count: 4
       assert_select 'li', text: "Name can't be blank"
-      assert_select 'li', text: "Email is invalid" 
-      assert_select 'li', text: "Email has already been taken"
+      assert_select 'li', text: "Email is invalid"
       assert_select 'li', text: "Password can't be blank"
       assert_select 'li', text: "Password confirmation doesn't match Password"
-      assert_select 'li', text: "Password is too short"
+    end
   end
 end
