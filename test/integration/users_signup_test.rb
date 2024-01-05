a1107d6910604e6fee87dd6fce8384d403c4da40
MarkLysenkoZp <marcuslysenko061@gmail.com>
2024-01-05 18:16:31 +0200

    Exercise 3

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,4 +21,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'li', text: "Password confirmation doesn't match Password"
     end
   end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_not flash[:success].empty?
+  end
 end
